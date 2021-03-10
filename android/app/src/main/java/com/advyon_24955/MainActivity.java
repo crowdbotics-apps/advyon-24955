a153d936b8cba32003c66bccf95d617c1a63d869
Crowdbotics <team@crowdbotics.com>
2021-03-10 20:47:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/advyon_24955/MainActivity.java
@@ -0,0 +1,15 @@
+package com.advyon_24955;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "advyon_24955";
+    }
+}
